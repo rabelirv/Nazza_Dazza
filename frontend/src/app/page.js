8ae022053d9eb9c4de07c9efe3e283fdd306274e
irabel-consultant <irabel-consultant@scholastic.com>
2023-08-03 09:15:39 -0700

    Created container for products

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,17 @@
+'use client'
+
+import {useEffect} from 'react';
 import NavBar from "./components/NavBar"
 import 'tailwindcss/tailwind.css'
 
+
 export default function Home() {
+
+  useEffect(()=>{
+      fetch("https://fakestoreapi.com/products")
+      .then(res => res.json())
+      .then(res=> console.log(res))
+  })
   return (
    <>
     <div className="flex border-2 border-orange-400">
